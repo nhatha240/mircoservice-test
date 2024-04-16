7d737c18e18c9a2346020ea1ea29f35c431ae12e
Hà Minh Nhật <nhatha@bravesoft.com.vn>
2024-04-16 09:45:29 +0700

    change

--- generated/aws/ec2_instance/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_instance_tfer--i-0268117a1af4f723d_terraform-dev-test_id" {
-  value = "${aws_instance.tfer--i-0268117a1af4f723d_terraform-dev-test.id}"
-}
--- generated/aws/s3/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_s3_bucket_tfer--test-s3-dev-terraform_id" {
-  value = "${aws_s3_bucket.tfer--test-s3-dev-terraform.id}"
-}
